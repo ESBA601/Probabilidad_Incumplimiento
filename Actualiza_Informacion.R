74e8c7605754059fe9e0aa44c33c3e2fc2599fef
ESBA601 <erich_18@hotmail.com>
2024-08-19 15:24:18 -0430

    Se actualiza ambas fechas con la del mes de estudio.
    En la primera fecha "FECHA0", se coloca el inicio de la fecha del mes de estuio.
    
    En la segunda fecha "FECHA1", se coloca el fin de la fecha del mes de estuio.

---
+++ Actualiza_Informacion.R
@@ -0,0 +1,9 @@
+
+#Sirve para definir formato en todos los Documetos y Titulos
+#Actualiza la fecha en los calculos
+
+FECHA0 <- as.Date("2024-04-01","%Y-%m-%d") ##YYYY-MM-DD
+
+#Sirve para Calculos de los clientes que se encuentran fuera de plazo
+
+FECHA1 <- as.Date("2024-04-30","%Y-%m-%d") ##YYYY-MM-DD
